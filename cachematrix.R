78f718848d9fcb29d5621380dbc6f9b7bcc61982
dmac <dmcintyre.4@gmail.com>
2016-08-06 23:45:50 -0400

    Completed & Tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Welcome to my R Programming Assignment. Please make yourself comfortable!
+## These functions a pretty much the same as the functions in the example...
+## Except they calculate the inverse of a matrix instead of the mean of a vector.
+## Crazy right?
 
-## Write a short comment describing this function
+## Let's look into makeCacheMatrix first, shall we?
+## It's full of "getters" and "setters" that get/set the value of the matrix
+## and it's inverse.
 
-## I don't know what I'm doing...
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSovle checks to see if the inverse has already been cached.
+## If not, it knows what to do. It knows... too much.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data.")
+    
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
